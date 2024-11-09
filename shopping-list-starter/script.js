4ffb483212d86a8531d19e1d62f832fee7533695
Your Name <you@example.com>
2024-11-09 15:25:31 +0800

    Added function to clear list

--- shopping-list-starter/script.js
+++ shopping-list-starter/script.js
@@ -1,16 +1,25 @@
 let shoppingListItems = ["milk", "eggs", "bread"];
+const newItemText = document.getElementById("new-item-text");
 
-updateItems();
+const clearInput = () => {
+  newItemText.value = "";
+};
+
+const clearList = () => {
+  const listElement = document.getElementById("shopping-list-items");
+  listElement.innerHTML = "";
+};
 
 const addItem = () => {
-  let item = document.getElementById("new-item-text").value;
+  const item = document.getElementById("new-item-text").value;
+  //Deconstruct array using ...
   shoppingListItems = [...shoppingListItems, item];
   updateItems();
 };
 
 const updateItems = () => {
-  // First we get the list element
-  let listElement = document.getElementById("shopping-list-items");
+  // First we get the list element - (already used this code in clearList but not sure how to make it DRY)
+  const listElement = document.getElementById("shopping-list-items");
   // Then we clear it of any existing items
   listElement.innerHTML = "";
 
@@ -20,4 +29,7 @@ const updateItems = () => {
     itemElement.innerText = shoppingItem;
     listElement.appendChild(itemElement);
   }
+  clearInput();
 };
+
+updateItems();
