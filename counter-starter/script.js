bf003657742b4a36eacec20285cddceef400ff06
Your Name <you@example.com>
2024-11-09 12:36:32 +0800

    Added subtraction and reset to counter and shopping list content

--- counter-starter/script.js
+++ counter-starter/script.js
@@ -1,8 +1,19 @@
 const add = (step) => {
   // We gave the number element an id of number so we can use that to get the element
-  let numberElement = document.getElementById("number");
+  const numberElement = document.getElementById("number");
   // We can use the innerText property to get the text inside the element
-  let number = numberElement.innerText;
-  number = parseInt(number) + step;
-  numberElement.innerText = number;
+  const number = parseInt(numberElement.innerText);
+  numberElement.innerText = number + step;
+};
+
+const subtract = (step) => {
+  const numberElement = document.getElementById("number");
+  const number = parseInt(numberElement.innerText);
+  numberElement.innerText = number - step;
+};
+
+const reset = () => {
+  const numberElement = document.getElementById("number");
+  //   const number = parseInt(numberElement.innerText);
+  numberElement.innerText = "0";
 };
